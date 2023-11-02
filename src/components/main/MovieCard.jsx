238c3b8d0c738e96e513af76c3c583c08bc181c0
FevenSeyfu <fevenseyfu00@gmail.com>
2023-11-02 15:34:14 +0300

    Restructure movie List

--- src/components/main/MovieCard.jsx
+++ src/components/main/MovieCard.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import '../../Assets/css/MovieCard.css'
-import { Rating,Card,Image,Pagination } from 'semantic-ui-react'
+import { Rating,Card,Image } from 'semantic-ui-react'
 
 import image1 from '../../Assets/images/avatar 1.jpg'
 import image2 from '../../Assets/images/avatar 2.jpg'
@@ -8,7 +8,6 @@ import image2 from '../../Assets/images/avatar 2.jpg'
 const MovieCard = () => {
   return (
     <>
-      <Card.Group itemsPerRow={4} className='centered stackable card-group'>
         <Card className='movie-card'>
           <Image src={image1} wrapped ui={false} />
           <Card.Content className='card-content'>
@@ -104,17 +103,6 @@ const MovieCard = () => {
               <Rating icon='star' defaultRating={3} maxRating={5} />
           </Card.Content>
         </Card>
-      </Card.Group>
-      <Pagination 
-        className="pagination"
-        boundaryRange={0}
-        defaultActivePage={1}
-        ellipsisItem={null}
-        firstItem={null}
-        lastItem={null}
-        siblingRange={1}
-        totalPages={5}
-    />
   </>
   )
 }
--- src/components/main/MovieList.jsx
+++ src/components/main/MovieList.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
 import MovieCard from './MovieCard'
-
+import '../../Assets/css/MovieCard.css'
+import {Card,Pagination } from 'semantic-ui-react'
 const MovieList = () => {
   return (
-      <MovieCard />
+     <div>
+        <Card.Group itemsPerRow={4} className='centered stackable card-group'>
+          <MovieCard />
+        </Card.Group>
+        <Pagination 
+          className="pagination"
+          boundaryRange={0}
+          defaultActivePage={1}
+          ellipsisItem={null}
+          firstItem={null}
+          lastItem={null}
+          siblingRange={1}
+          totalPages={5}
+      />
+     </div>
   )
 }
 
