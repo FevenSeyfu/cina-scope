7851e41b5ea46e6d3c12435d85635c0c8d21f4d9
FevenSeyfu <fevenseyfu00@gmail.com>
2023-11-06 13:42:49 +0300

    style error message

--- src/components/main/Pages/Error.jsx
+++ src/components/main/Pages/Error.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from 'react';
+import { Container, Header, Icon } from 'semantic-ui-react';
+
 
 const Error = () => {
+  const styleContainer={
+    marginTop:'10rem',
+  }
   return (
-    <h1>Error</h1>
-  )
-}
-
+    <Container textAlign="center" style={styleContainer} >
+      <Header as="h1" icon inverted color='red'>
+        <Icon name="exclamation triangle" />
+        404 Error: Page Not Found
+        <Header.Subheader >The page you're looking for doesn't exist.</Header.Subheader>
+      </Header>
+    </Container>
+  );
+};
 export default Error
