46a96a81a09bc23e5bcb7d205cc32056c52c50f6
FevenSeyfu <fevenseyfu00@gmail.com>
2023-11-06 12:26:01 +0300

    Complete search Page

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,18 +1,12 @@
 import React,{useState} from 'react'
 import logo  from '../../Assets/images/logo.png'
-import Search from '../main/Pages/Search';
+import { Menu,Icon } from 'semantic-ui-react';
 
 const Header = () => {
-  const [searchValue, setSearchValue] = useState('');
-
-  const HandleSearchChange = (e) => {
-    setSearchValue(e.target.value)
+  const [activeItem, setActiveItem] = useState('');
+  const handleItemClick = (e,{name}) => {
+    setActiveItem(name);
   }
-  
-  const handleSearchSubmit = (e) => {
-    <Search  searchValue={searchValue}/>
-  }
-
   return (
     <div className='top-header'>
       <div className="logo-container">
@@ -21,15 +15,50 @@ const Header = () => {
         </a>
       </div>
       <div class="ui grid">
-        <div class="two wide column">
-          <div class="ui search">
-            <form class="ui icon input" onSubmit={handleSearchSubmit}>
-              <input type="text" autoComplete="off" value={searchValue} placeholder="Search..." onChange={HandleSearchChange} tabindex="0" class="prompt"/>
-                <i aria-hidden="true" class="search icon"></i>
-            </form>
-          </div>
+        <div class="">
+        <Menu pointing secondary stackable inverted >
+          <Menu.Item 
+            as='a' href='/'
+            name='trending'
+            active={activeItem === 'trending'}
+            onClick={handleItemClick}
+            >
+            <Icon name='fire' />
+            Trending
+          </Menu.Item>
+          <Menu.Item 
+            as='a' 
+            href='/movies'
+            name='movies'
+            active={activeItem === 'movies'}
+            onClick={handleItemClick}>
+            <Icon name='film' />
+            Movies
+          </Menu.Item>
+          <Menu.Item 
+            as='a' 
+            href='/TV'
+            name='tv'
+            active={activeItem === 'tv'}
+            onClick={handleItemClick}>
+            <Icon name='tv' />
+            TV Series
+          </Menu.Item>
+          <Menu.Item 
+            as='a' 
+            href='/search'
+            name='search'
+            active={activeItem === 'search'}
+            onClick={handleItemClick}>
+            <Icon name='search' />
+            Search
+          </Menu.Item>
+        </Menu>
         </div>
      </div>
+
+    
+
     </div> 
 
   )
--- src/components/main/Pages/Search.jsx
+++ src/components/main/Pages/Search.jsx
@@ -1,43 +1,41 @@
 import React,{useState,useEffect} from "react";
 import MovieCard from '../../main/MovieCard'
-import  fetchData from '../../../API/fetchData'
-import { Header,Icon } from 'semantic-ui-react'
+import  MovieSearch from '../../../API/MovieSearch'
 
-const Search = ({searchValue}) => {
+const Search = () => {
   const [movies, setMovies] = useState([]);
   const [activePage, setActivePage] = useState(1);
   const [totalPages, setTotalPages] = useState(1);
+  const [searchQuery, setSearchQuery] = useState('');
 
-  const apiKey = '37f9159ef25a3d939000ae37af4753b0';
-  const apiURL=`https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&page=${activePage}&query=${searchValue}`
- 
-  useEffect(() => {    
-    fetchData(apiURL,setMovies,setTotalPages);
-  }, [activePage]);
-  
   const handlePaginationChange = (e, { activePage }) => {
     setActivePage(activePage);
   };
-  const headerStyle = {
-    display:'flex',
-    flexDirection: 'row',
-    justifyContent:'center',
-    color:'#F5F5F5' ,
-    padding: '0.3rem 0'
+  const handleSearchChange = (event) => {
+    setSearchQuery(event.target.value);
+
   };
 
+  
   const HeaderTop = () => {
     return(
-        <Header as='h1' style={headerStyle}>
-          <Icon color='red' name='search' />
-          search Result
-          <Icon color='red' name='search' />
-        </Header>
+      <div class="ui search">
+      <form class="ui icon input" >
+        <input type="text" 
+              autoComplete="off" 
+              placeholder="Search..." 
+              value={searchQuery}
+              onChange={handleSearchChange}
+              tabindex="0" 
+              class="prompt"/>
+          <i aria-hidden="true" class="search icon"></i>
+      </form>
+      
+    </div>
     )
   }
   return (
-     <div>
-        
+     <div> <MovieSearch searchQuery={searchQuery} setMovies={setMovies} />
           <MovieCard 
             movies = {movies}
             activePage={activePage}
